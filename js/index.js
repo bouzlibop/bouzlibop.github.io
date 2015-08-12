f90eafa979de5bf7d4448ae6ee32d4df12d292bf
Adrian Pawel Baran <adrian.pawel.baran@cern.ch>
2015-08-12 23:55:43 +0200

    Responsive Layout part 1

--- js/index.js
+++ js/index.js
@@ -1,16 +1,17 @@
-function toogleWidth(element) {
-  if (+element.style.width.slice(0, -1) > 0) {
-    element.style.width = '0%';
+function toogle(element) {
+  if (element.style.opacity === "" || element.style.opacity === '0') {
+    element.style.opacity = '1';
   } else {
-    element.style.width = '100%';
+    element.style.opacity = '0';
   }
 }
 
 document.addEventListener("DOMContentLoaded", function() {
   transformicons.add('.tcon');
+
   var menuButton = document.getElementsByClassName('tcon')[0];
   var navbar = document.getElementsByClassName('navbar')[0];
   menuButton.addEventListener('click', function (event) {
-    toogleWidth(navbar);
+    toogle(navbar);
   });
 });
